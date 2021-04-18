ffb44bb611f152df707f96cfaf2bafcfefa8e98d
GwangJin Byeon <qusrhkdwls45@gmail.com>
2021-04-19 00:58:03 +0900

    update : 가닥 잡음. 포지션만 관리하고 모든 좌표에 객체를 추가하는 식으로 개발하자.

--- assets/wormGameEngin.js
+++ assets/wormGameEngin.js
@@ -1,1 +1,17 @@
-console.log(123);
+var Object = function Object(id, size){
+  this.id = id;
+  this.size = size;
+  killWorm = false;
+}
+
+Object.prototype.create = function create(){
+  
+}
+
+var DimensionalArray = function DimensionalArray(){
+  
+}
+
+DimensionalArray.prototype.set = function(){
+  
+}
