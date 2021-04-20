fc1f26f6ca55a3aea5a58e22ea3e50c43b4d8d3f
GwangJin Byeon <qusrhkdwls45@gmail.com>
2021-04-21 00:05:44 +0900

    update : api 개발 중

--- assets/wormGameEngin.js
+++ assets/wormGameEngin.js
@@ -1,96 +1,159 @@
-var wgObject = function wgObject( id, position ){
-  this.id = id;
-  this.position = position;
-  killWorm = false;
-  type = WgObjType.wgObject;
+function WormGameEngine( size ){
+  this.score = 0;
+  this.time = 0;
+  this.field = new WormGameEngine.Field( "Field", size );
+  this.startKey = null;
 }
 
-wgObject.prototype.moveByPosition = function remove( position ){
+WormGameEngine.prototype.addObject = function addObject( wgObject ){
+  var object = this.field.getObjectById( wgObject.id );
+
+  console.assert( object, {msg : "중복되는 아이디의 객체를 추가할 수 없습니다."} );
+
+  this.field.add( wgObject );
+}
+
+WormGameEngine.prototype.start = function start(){
+  var self = this;
+  this.field.worms.forEach(function( worm ){
+    worm.autoMove = true;
+  });
+
+  this.startKey = setInterval(function(){
+    // worms가 한칸 씩 이동
+    this.field.moveWorms();
+
+    // worm들의 방향 변경
+    this.field.setWormsDirection();
+  }, 1000);
+}
+
+WormGameEngine.prototype.pause = function pause(){
+  this.field.worms.forEach(function( worm ){
+    worm.autoMove = false;
+  });
+
+  clearInterval( this.startKey );
+}
+
+WormGameEngine.prototype.setWormDirection = function setWormDirection( direction ){
+  this.field.setWormHeadDirection( direction );
+}
+
+// 1. 객체 공통 클래스
+WormGameEngine.WgObject = function WgObject( id, position ){
+  this.id = id;
+  this.position = position;
+  isKillObj = false;
+  isEdible = false;
+  type = WormGameEngine.WgObjType.WgObject;
+}
+
+WormGameEngine.WgObject.prototype.moveByPosition = function remove( position ){
   this.potition = position;
   return this;
 }
 
-wgObject.prototype.moveright = function moveRight(){
+WormGameEngine.WgObject.prototype.moveright = function moveRight(){
   this.position.x++;
   return this;
 }
 
-wgObject.prototype.moveleft = function moveLeft(){
+WormGameEngine.WgObject.prototype.moveleft = function moveLeft(){
   this.position.x--;
   return this;
 }
 
-wgObject.prototype.movetop = function moveTop(){
+WormGameEngine.WgObject.prototype.movetop = function moveTop(){
   this.position.y++;
   return this;
 }
 
-wgObject.prototype.movebottom = function moveBottom(){
+WormGameEngine.WgObject.prototype.movebottom = function moveBottom(){
   this.position.y--;
   return this;
 }
 
 /*
-객체의 이동 방향을 나타내기 위한 enum
+2. 다차원 배열을 생성하기 위한 객체. ui가 없이 솔루션을 통해 개발된 지렁이게임을 콘솔창에서 확인하기 위해서 만든 객체이다.
 */
-var MoveDirection = {right: "right", left: "left", top: "top", bottom: "bottom"};
-
-/*
-객체의 Type에 대한 enum
-*/
-var WgObjType = {wgObject: "wgObject", worm: "worm", obstacle: "obstacle",  food: "food", field: "field", wall: "wall"};
-
-/*
-다차원 배열을 생성하기 위한 객체. ui가 없이 솔루션을 통해 개발된 지렁이게임을 콘솔창에서 확인하기 위해서 만든 객체이다.
-*/
-var DimensionalArray = function DimensionalArray(){
+WormGameEngine.DimensionalArray = function DimensionalArray(){
 
 }
 
-DimensionalArray.prototype.set = function(){
+WormGameEngine.DimensionalArray.prototype.set = function(){
 
 }
 
-var Worm = function Worm( id, position ){
-  wgObject.apply( this, arguments );
+/*
+3. 객체의 이동 방향을 나타내기 위한 enum
+*/
+WormGameEngine.MoveDirection = {right: "right", left: "left", top: "top", bottom: "bottom"};
+
+/*
+4. 객체의 Type에 대한 enum
+*/
+WormGameEngine.WgObjType = {wgObject: "wgObject", worm: "worm", obstacle: "obstacle",  food: "food", field: "field"};
+
+// 5. 지렁이 클래스
+WormGameEngine.Worm = function Worm( id, position ){
+  WormGameEngine.WgObject.apply( this, arguments );
   this.head = false;
-  this.direction = MoveDirection.right;
+  this.direction = WormGameEngine.MoveDirection.right;
   this.autoMove = false;
-  this.type = WgObjType.worm;
-  this.killWorm = true;
+  this.type = WormGameEngine.WgObjType.worm;
+  this.isKillObj = true;
 }
-Worm.prototype = Object.create( wgObject.prototype );
-Worm.prototype.constructor = Worm;
+WormGameEngine.Worm.prototype = Object.create( WormGameEngine.WgObject.prototype );
+WormGameEngine.Worm.prototype.constructor = WormGameEngine.Worm;
 
-Worm.prototype.autoMove = function autoMove(){
+WormGameEngine.Worm.prototype.autoMove = function autoMove(){
   this[ "move" + this.direction ]();
   return this;
 }
 
-var Field = function Field( id, size ){
+// 6. 먹이 클래스
+WormGameEngine.Food = function Food( id, position ){
+  WormGameEngine.WgObject.apply( this, arguments );
+  isEdible = true;
+}
+WormGameEngine.Food.prototype = Object.create( WormGameEngine.WgObject.prototype );
+WormGameEngine.Food.prototype.constructor = WormGameEngine.Food;
+
+// 7. 장애물 클래스
+WormGameEngine.Obstacle = function Obstacle( id, position ){
+  WormGameEngine.WgObject.apply( this, arguments );
+  this.isKillObj = true;
+  this.type = WormGameEngine.WbObjType.obstacle;
+}
+WormGameEngine.Obstacle.prototype = Object.create( WormGameEngine.WgObject.prototype );
+WormGameEngine.Obstacle.prototype.constructor = WormGameEngine.Obstacle;
+
+// 8. 필드 클래스
+WormGameEngine.Field = function Field( id, size ){
   this.id = id;
   this.size = size;
   this.children = {};
   this.worms = [];
-  this.type = WgObjType.field;
+  this.type = WormGameEngine.WgObjType.field;
 }
 
-Field.prototype.add = function add( obj ){
+WormGameEngine.Field.prototype.add = function add( obj ){
   this.children[obj.id] = obj;
   return obj;
 }
 
-Field.prototype.getObjectById = function getObjectById( id ){
+WormGameEngine.Field.prototype.getObjectById = function getObjectById( id ){
   return this.children.id;
 }
 
-Field.prototype.removeObjectById = function removeObjectById( id ){
+WormGameEngine.Field.prototype.removeObjectById = function removeObjectById( id ){
   var type = this.children.id.type;
   // children에서 삭제
   delete this.children.id;
   
   // Worm 인스터스가 아니면 삭제 하지 않는다.
-  if( !(WgObjType[type] == WgObjType.worm) ){
+  if( !(WormGameEngine.WgObjType[type] == WormGameEngine.WgObjType.worm) ){
     return;
   }
   
@@ -105,13 +168,13 @@ Field.prototype.removeObjectById = function removeObjectById( id ){
   this.worms.splice( objIndex, 1 );
 }
 
-Field.prototype.setWormHeadDirection = function setWormHeadDirection( direction ){
+WormGameEngine.Field.prototype.setWormHeadDirection = function setWormHeadDirection( direction ){
   // 지렁이 머리 객체의 위치를 set한다.
   this.worms[ 0 ].direction = direction;
   return this.worms;
 }
 
-Field.prototype.moveWorms = function moveWorms(){
+WormGameEngine.Field.prototype.setWormsDirection = function setWormsDirection(){
   for( var i = 1; i < this.worms.length; i++ ){
     this.worms[i].direction = this.worms[i-1].direction;
   }
@@ -119,39 +182,45 @@ Field.prototype.moveWorms = function moveWorms(){
   return this.worms;
 }
 
-Field.prototype.setEdgeWalls = function setEdgeWalls(){
+WormGameEngine.Field.prototype.setEdgeObstacles = function setEdgeObstacles(){
   var xSize = this.size.x;
   var ySize = this.size.y;
   // 상단 x 축
   for( var i=0; i < xSize - 1; i++ ){
-    this.children[ "x_top_" + i ] = new Wall( "x_top_" + i, {x:i, y:0} );
+    this.children[ "x_top_" + i ] = new WormGameEngine.Obstacle( "x_top_" + i, {x:i, y:0} );
   }
   
   // 하단 x 축
   for( var i=0; i < xSize - 1; i++ ){
-    this.children[ "x_bottom_" + i ] = new Wall( "x_bottom_" + i, {x:i, y:ySize-1} );
+    this.children[ "x_bottom_" + i ] = new WormGameEngine.Obstacle( "x_bottom_" + i, {x:i, y:ySize-1} );
   }
   
   // 왼쪽 y 축
   for( var i=0; i < ySize - 1; i++ ){
-    this.children[ "y_left_" + i ] = new Wall( "y_left_" + i, {x:0, y:i} );
+    this.children[ "y_left_" + i ] = new WormGameEngine.Obstacle( "y_left_" + i, {x:0, y:i} );
   }
   
   // 오른쪽 y 축
   for( var i=0; i < ySize - 1; i++ ){
-    this.children[ "y_left_" + i ] = new Wall( "y_left_" + i, {x:xSize-1, y:i} );
+    this.children[ "y_left_" + i ] = new WormGameEngine.Obstacle( "y_left_" + i, {x:xSize-1, y:i} );
   }
   
   return this.children;
 }
 
-var Obstacle = function Obstacle( id, position ){
-  wgObject.apply( this, arguments );
-  this.killWorm = true;
-  this.type = WbObjType.obstacle;
+WormGameEngine.Field.prototype.moveWorms = function moveWorms(){
+  var isKillWorm = true;
+  var self = this;
+  // worms가 한칸 씩 이동
+  this.worms.forEach(function( worm ){
+    worm.autoMove();
+  });
+
+  for(var item in this.children){
+    if( JSON.stringify(this.children[item].position) == JSON.stringify(this.worms[0]) ){
+      return true;
+    }
+  }
+
+  return false;
 }
-Obstacle.prototype = Object.create( wgObject.prototype );
-Obstacle.prototype.constructor = Obstacle;
-
-
-
