385f4f106287174aa8cd953579fdbf6ab1433af2
GwangJin Byeon <qusrhkdwls45@gmail.com>
2021-04-20 10:56:01 +0900

    Update wormGameEngin.js

--- assets/wormGameEngin.js
+++ assets/wormGameEngin.js
@@ -38,7 +38,7 @@ var MoveDirection = {right: "right", left: "left", top: "top", bottom: "bottom"}
 /*
 객체의 Type에 대한 enum
 */
-var WgObjType = {wgObject: "wgObject", worm: "worm", stone: "stone", wall: "wall", food: "food", field: "field"};
+var WgObjType = {wgObject: "wgObject", worm: "worm", obstacle: "obstacle",  food: "food", field: "field", wall: "wall"};
 
 /*
 다차원 배열을 생성하기 위한 객체. ui가 없이 솔루션을 통해 개발된 지렁이게임을 콘솔창에서 확인하기 위해서 만든 객체이다.
@@ -74,13 +74,9 @@ var Field = function Field( id, size ){
   this.worms = [];
   this.type = WgObjType.field;
 }
-Field.prototype = wgObject.create( wgObject.prototype );
-Field.prototype.constructor = Field;
 
 Field.prototype.add = function add( obj ){
   this.children[obj.id] = obj;
-  
-  
   return obj;
 }
 
@@ -98,7 +94,7 @@ Field.prototype.removeObjectById = function removeObjectById( id ){
     return;
   }
   
-  // Worm 인스턴스라면 삭제 필요
+  // Worm 인스턴스라면 삭제 필요(this.worms 배열에서 제거)
   var objIndex;
   this.worms.forEach(function( item, i ){
     if( item.id == id ){
@@ -109,7 +105,53 @@ Field.prototype.removeObjectById = function removeObjectById( id ){
   this.worms.splice( objIndex, 1 );
 }
 
-Field.prototype.setWormDirection = function setWormDirection( direction ){
+Field.prototype.setWormHeadDirection = function setWormHeadDirection( direction ){
   // 지렁이 머리 객체의 위치를 set한다.
-  this.worms[0].direction = direction;
+  this.worms[ 0 ].direction = direction;
+  return this.worms;
 }
+
+Field.prototype.moveWorms = function moveWorms(){
+  for( var i = 1; i < this.worms.length; i++ ){
+    this.worms[i].direction = this.worms[i-1].direction;
+  }
+  
+  return this.worms;
+}
+
+Field.prototype.setEdgeWalls = function setEdgeWalls(){
+  var xSize = this.size.x;
+  var ySize = this.size.y;
+  // 상단 x 축
+  for( var i=0; i < xSize - 1; i++ ){
+    this.children[ "x_top_" + i ] = new Wall( "x_top_" + i, {x:i, y:0} );
+  }
+  
+  // 하단 x 축
+  for( var i=0; i < xSize - 1; i++ ){
+    this.children[ "x_bottom_" + i ] = new Wall( "x_bottom_" + i, {x:i, y:ySize-1} );
+  }
+  
+  // 왼쪽 y 축
+  for( var i=0; i < ySize - 1; i++ ){
+    this.children[ "y_left_" + i ] = new Wall( "y_left_" + i, {x:0, y:i} );
+  }
+  
+  // 오른쪽 y 축
+  for( var i=0; i < ySize - 1; i++ ){
+    this.children[ "y_left_" + i ] = new Wall( "y_left_" + i, {x:xSize-1, y:i} );
+  }
+  
+  return this.children;
+}
+
+var Obstacle = function Obstacle( id, position ){
+  wgObject.apply( this, arguments );
+  this.killWorm = true;
+  this.type = WbObjType.obstacle;
+}
+Obstacle.prototype = wgObject.create( wgObject.prototype );
+Obstacle.prototype.constructor = Obstacle;
+
+
+
