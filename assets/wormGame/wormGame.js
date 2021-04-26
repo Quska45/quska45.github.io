9cdc12843ad89221ff5da42e011b4ead3c6b4113
GwangJin Byeon <qusrhkdwls45@gmail.com>
2021-04-26 15:25:09 +0900

    Update wormGame.js

--- assets/wormGame/wormGame.js
+++ assets/wormGame/wormGame.js
@@ -4,7 +4,7 @@ var fieldSize = {x: 50, y:50};
 var wg = new WormGameEngine( fieldSize );
 var arrMaker = new WormGameEngine.DimensionalArray( fieldSize );
 
-
+$viewer.text( "이게 된다?" );
 $viewer.text( arrMaker.printField(wg.field) );
 
 
